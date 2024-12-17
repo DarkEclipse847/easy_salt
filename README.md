0a9a3c34ff6324478a58b3dd090e159be3f2abcb
DarkEclipse847 <ilyapetrov847@gmail.com>
2024-12-17 20:15:37 +0300

    Added support for sha3, added readme, fixed bug with sha-384

---
+++ README.md
@@ -0,0 +1,47 @@
+# easy_salt 0.1.1
+
+This is a simple crate providing salt for commonly used hashing algorithms.
+This crate will give you opportunity to generate **tuple** of type `(hash, salt)` for your needs.
+
+`__All of the code was written on Android phone using Termux. :)__`
+
+## List of hashing algorithms
+Unchecked algorithms will be added soon
+ - [x] SHA:
+     - [x] sha-1
+     - [x] sha-224
+     - [x] sha-256
+     - [x] sha-384
+     - [x] sha-512
+     - [x] sha3-224
+     - [x] sha3-256
+     - [x] sha3-384
+     - [x] sha3-512
+ - [ ] MD:
+     - [ ] md2
+     - [ ] md4
+     - [ ] md5
+ - [ ] Bcrypt
+ - [ ] Argon2:
+     - [ ] Argon2d
+     - [ ] Argon2i
+     - [ ] Argon2id
+
+## Example
+ ```
+ extern crate easy_salt;
+ use easy_salt::salty_sha::*;
+
+ fn main(){
+     let length: u64 = 8;
+     let hash = salty_sha256("hello", length);
+     println!("Hash: {:?}, Salt: {:?}", hash.0, hash.1);
+
+     let another_hash = salty_sha256("hello", length);
+     println!("Second Hash: {:?}, Second salt: {:?}", another_hash.0, another_hash.1);
+
+     assert_ne!(hash.0, another_hash.0);
+ }
+ ```
+
+There is a plan to change logic so instead of tuple functions will return struct with fields `hash` and `salt` respectfully for better r>
--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,16 @@
 //! This crate will give you opportunity to generate **tuple** of type `(hash, salt)` for your needs.
 //!
 //! ## List of hashing algorithms
-//! - [ ] SHA:
+//! - [x] SHA:
 //!     - [x] sha-1
 //!     - [x] sha-224
 //!     - [x] sha-256
 //!     - [x] sha-384
 //!     - [x] sha-512
-//!     - [ ] sha3-224
-//!     - [ ] sha3-256
-//!     - [ ] sha3-384
-//!     - [ ] sha3-512
+//!     - [x] sha3-224
+//!     - [x] sha3-256
+//!     - [x] sha3-384
+//!     - [x] sha3-512
 //! - [ ] MD:
 //!     - [ ] md2
 //!     - [ ] md4
@@ -101,6 +101,22 @@ pub mod salty_sha{
         let result = sha512(&str.to_string()).to_hex_string();
         result
     }
+    fn to_sha3_224(str: &str) -> String{ 
+        let result = sha3_224(&str.to_string()).to_hex_string();
+        result
+    }
+    fn to_sha3_256(str: &str) -> String{ 
+        let result = sha3_256(&str.to_string()).to_hex_string();
+        result
+    }
+    fn to_sha3_384(str: &str) -> String{ 
+        let result = sha3_384(&str.to_string()).to_hex_string();
+        result
+    }
+    fn to_sha3_512(str: &str) -> String{ 
+        let result = sha3_512(&str.to_string()).to_hex_string();
+        result
+    }
 
     /// Function, which returns sha-1 salted hash
     /// Salt is added to the beginning of the string
@@ -176,7 +192,7 @@ pub mod salty_sha{
     /// ```
     pub fn salty_sha384(str: &str, salt_length: u64) -> (String, String){
         let salt = generate_salt(salt_length);
-        let encrypted_string = to_sha256(&(salt.to_string() + str));
+        let encrypted_string = to_sha384(&(salt.to_string() + str));
         return (encrypted_string, salt);
     }
 
@@ -196,6 +212,84 @@ pub mod salty_sha{
     pub fn salty_sha512(str: &str, salt_length: u64) -> (String, String){
         let salt = generate_salt(salt_length);
         let encrypted_string = to_sha512(&(salt.to_string() + str));
+        return (encrypted_string, salt);
+    }
+    
+    
+    /// Generates salted sha3-224 hash
+    /// Salt is added to the beginning of the string
+    ///
+    /// ##Example
+    /// ```
+    /// extern crate easy_salt;
+    /// use easy_salt::salty_sha::salty_sha3_224;
+    /// 
+    /// fn main(){
+    ///     let hash = salty_sha3_224("some string", 6); //first argument is string you need to be hashed, second argument is the salt length
+    ///     println!("Hash: {:?}, Salt: {:?}", hash.0, hash.1); //Note, that variable hash is a tuple, so it returns (hash, salt)
+    /// }
+    /// ```
+    pub fn salty_sha3_224(str: &str, salt_length: u64) -> (String, String){
+        let salt = generate_salt(salt_length);
+        let encrypted_string = to_sha3_224(&(salt.to_string() + str));
+        return (encrypted_string, salt);
+    }
+    
+    /// Generates salted sha3-256 string
+    /// Returns tuple (hash, salt)
+    /// Salt is added to the beginning of the string
+    ///
+    /// ##Example
+    /// ```
+    /// extern crate easy_salt;
+    /// use easy_salt::salty_sha::salty_sha3_256;
+    /// 
+    /// fn main(){
+    ///     let hash = salty_sha3_256("some string", 6); //first argument is string you need to be hashed, second argument is the salt length
+    ///     println!("Hash: {:?}, Salt: {:?}", hash.0, hash.1); //Note, that variable hash is a tuple, so it returns (hash, salt)
+    /// }
+    /// ```
+    pub fn salty_sha3_256(str: &str, salt_length: u64) -> (String, String){
+        let salt = generate_salt(salt_length);
+        let encrypted_string = to_sha3_256(&(salt.to_string() + str));
+        return (encrypted_string, salt);
+    }
+
+    /// Function, which returns sha3-384 salted hash
+    /// Salt is added to the beginning of the string
+    ///
+    /// ##Example
+    /// ```
+    /// extern crate easy_salt;
+    /// use easy_salt::salty_sha::salty_sha3_384;
+    /// 
+    /// fn main(){
+    ///     let hash = salty_sha3_384("some string", 6); //first argument is string you need to be hashed, second argument is the salt length
+    ///     println!("Hash: {:?}, Salt: {:?}", hash.0, hash.1); //Note, that variable hash is a tuple, so it returns (hash, salt)
+    /// }
+    /// ```
+    pub fn salty_sha3_384(str: &str, salt_length: u64) -> (String, String){
+        let salt = generate_salt(salt_length);
+        let encrypted_string = to_sha3_384(&(salt.to_string() + str));
+        return (encrypted_string, salt);
+    }
+
+    /// Function, which returns sha3-512 salted hash
+    /// Salt is added to the beginning of the string
+    ///
+    /// ##Example
+    /// ```
+    /// extern crate easy_salt;
+    /// use easy_salt::salty_sha::salty_sha3_512;
+    /// 
+    /// fn main(){
+    ///     let hash = salty_sha3_512("some string", 6); //first argument is string you need to be hashed, second argument is the salt length
+    ///     println!("Hash: {:?}, Salt: {:?}", hash.0, hash.1); //Note, that variable hash is a tuple, so it returns (hash, salt)
+    /// }
+    /// ```
+    pub fn salty_sha3_512(str: &str, salt_length: u64) -> (String, String){
+        let salt = generate_salt(salt_length);
+        let encrypted_string = to_sha3_512(&(salt.to_string() + str));
         return (encrypted_string, salt);
     }
 }
